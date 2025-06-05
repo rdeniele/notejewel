79181fad3e71ea158d354538eef97b1c37d27f13
Ron Deniele D. Paragoso <114451287+rdeniele@users.noreply.github.com>
2025-06-05 19:34:32 +0800

    fix: update glowColor for improved shadow effect

--- src/styles/utils.ts
+++ src/styles/utils.ts
@@ -1,3 +1,3 @@
-const glowColor = "rgba(50,130,246,0.3)";
+const glowColor = "rgba(218, 165, 32, 0.3)";
 
 export const shadow = `0 0 5px ${glowColor}, 0 0 10px ${glowColor}, 0 0 15px ${glowColor}, 0 0 20px ${glowColor}`;
