3e98088c7dffec8e29ad890e0044dc83902c699c
Ron Deniele D. Paragoso <114451287+rdeniele@users.noreply.github.com>
2025-06-05 23:20:59 +0800

    trying to add google adsense script to the head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-adsense-account" content="ca-pub-4143521375584293" />
         <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4143521375584293"
@@ -17,4 +18,3 @@ export default function Document() {
     </Html>
   )
 }
-
